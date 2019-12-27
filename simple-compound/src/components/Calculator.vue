7b0703a446ad5e5aa0a9f6e69d6b969337f6866e
Joe <carre.joe3@gmail.com>
2019-12-27 12:37:34 +0000

    added minimum amount of fields

--- simple-compound/src/components/Calculator.vue
+++ simple-compound/src/components/Calculator.vue
@@ -1,6 +1,12 @@
 <template>
   <v-container>
-
+    <v-text-field label="Base amount" v-model="baseAmount"></v-text-field>
+    <v-text-field label="Interest rate" v-model="interestRate">
+      <v-icon slot="append" color="green">mdi-percent</v-icon>
+    </v-text-field>
+    <v-text-field label="Calculation period" v-model="calculationPeriod"></v-text-field>
+    <v-overflow-btn :items="dropdownCalculationPeriodOptions" label="Compound interval"></v-overflow-btn>
+    <v-text-field label="Regular monthly deposit" v-model="regularDeposit"></v-text-field>
   </v-container>
 </template>
 
@@ -8,6 +14,11 @@
 export default {
   name: 'Calculator',
   data: () => ({
+    baseAmount: 0,
+    interestRate: 0,
+    calculationPeriod: 0,
+    dropdownCalculationPeriodOptions: ['Daily', 'Monthly', 'Quarterly', 'Half yearly', 'Yearly'],
+    regularDeposit: 0
   })
 }
 </script>
