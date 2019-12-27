f9d46f1e95f08fa01e0c9f5832758f5377e769f9
Joe <carre.joe3@gmail.com>
2019-12-27 17:58:05 +0000

    years calculation period is now selected by default

--- simple-compound/src/components/Calculator.vue
+++ simple-compound/src/components/Calculator.vue
@@ -7,7 +7,11 @@
     <v-container>
       <v-row>
         <v-text-field label="Calculation period" v-model="calculationPeriodValue"></v-text-field>
-        <v-overflow-btn :items="calculationPeriodTypes" item-text="name" item-value="value" v-model="selectedCalculationPeriodTypeValue"></v-overflow-btn>
+        <!-- <v-overflow-btn :items="calculationPeriodTypes" item-text="name" item-value="value" v-model="selectedCalculationPeriodTypeValue"></v-overflow-btn> -->
+        <v-btn-toggle v-model="selectedCalculationPeriodTypeValue">
+          <v-btn value="1">Years</v-btn>
+          <v-btn value="12">Months</v-btn>
+        </v-btn-toggle>
       </v-row>
     </v-container>
     <v-overflow-btn :items="dropdownCalculationPeriodOptions" item-text="name" item-value="value" label="Compound interval" v-model="calculationPeriodInterval"></v-overflow-btn>
@@ -35,7 +39,7 @@ export default {
         value: 12
       }
     ],
-    selectedCalculationPeriodTypeValue: 1,
+    selectedCalculationPeriodTypeValue: '1',
     dropdownCalculationPeriodOptions:
     [
       {
